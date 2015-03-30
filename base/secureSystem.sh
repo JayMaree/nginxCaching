9ee306217eda0b0051fd7a200a6f28b48470d98e
Jay Maree <j.maree92@gmail.com>
2015-03-30 14:27:51 +0200

    Simplified the sed line

--- base/secureSystem.sh
+++ base/secureSystem.sh
@@ -2,4 +2,5 @@
 # This script will change a few configurations to secure the server ( just a little )
 
 # Let's harden the door
-sed -i 's/8585/22/g' /etc/network/interfaces
+sed -i 's:Port 22:Port 8585:g' /etc/network/interfaces
+sudo service ssh reload
