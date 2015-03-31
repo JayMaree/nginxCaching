71f86dc569cd231ed74576caa6b0ea4e11afbf84
Jay Maree <j.maree92@gmail.com>
2015-03-31 09:06:25 +0200

    README fixed

--- installPackages.sh
+++ installPackages.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Let's configure the packages
 # First of all, we have to configure the standard daemons/services
-sh /base/baseConfig.sh
+sh /etc/nginxCaching/base/baseConfig.sh
 
 # Let's set a static script folder
-script_dir="/etc/nginxCaching"
+script_dir="/etc/nginxCaching/done"
 if [ -d "$script_dir" ];
 then
 echo 'The nginxCaching folder exist already, please delete the folder and start over. If you update nginxCaching with new configuration files the installer may crash.'
 exit 1
 else
-sudo wget https://raw.github.com/jaymaree/nginxCaching/beta/configs/default.conf -O /etc/nginxCaching/configs/default.conf
-sudo wget https://raw.github.com/jaymaree/nginxCaching/beta/configs/nginx.conf -O /etc/nginxCaching/configs/nginx.conf
+sudo wget https://raw.githubusercontent.com/JayMaree/nginxCaching/beta/configs/changeme -O /etc/nginxCaching/configs/changeme
+sudo wget https://raw.githubusercontent.com/JayMaree/nginxCaching/beta/configs/nginx.conf -O /etc/nginxCaching/configs/nginx.conf
 # and all the other configs
 fi
 if [ $(dpkg-query -W -f='${Status}' sudo 2>/dev/null | grep -c "ok installed") -eq 0 ]
@@ -54,7 +54,7 @@ wget -O http://wordpress.org/latest.tar.gz /etc/nginxCaching/wordpress/latest.ta
 # code will come here
 
 # let's move the default config to nginx
-mv /etc/nginxCaching/configs/default.conf /etc/nginx/sites-available/changeme
+mv /etc/nginxCaching/configs/changeme /etc/nginx/sites-available/changeme
 # let's move the modified nginx configuration
 rm /etc/nginx/nginx.conf 
 mv /etc/nginxCaching/configs/nginx.conf /etc/nginx/nginx.conf
@@ -62,5 +62,8 @@ mv /etc/nginxCaching/configs/nginx.conf /etc/nginx/nginx.conf
 ln -s /etc/nginx/sites-available/changeme /etc/nginx/sites-enabled/
 service nginx reload
 
+# make installation done
+# will create a better solution later on
+mkdir /etc/nginxCaching/done
 
 
